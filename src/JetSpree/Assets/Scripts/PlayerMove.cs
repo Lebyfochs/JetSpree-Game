679150563457090097abfa6b662778b3d6c0f4ab
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2022-12-05 20:59:20 +0000

    Added movement
    Movement added plus physics to player and floor. Basic materials added also.

--- src/JetSpree/Assets/Scripts/PlayerMove.cs
+++ src/JetSpree/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private Rigidbody rb;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerSpeed = 40.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //This moves it forward in the direction of the rotation.
+            rb.AddForce(transform.forward * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //Despite saying forward it does move it back thanks to minus.
+            rb.AddForce(playerSpeed * Time.deltaTime * -transform.forward);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0.0f, -60f * Time.deltaTime, 0.0f, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0.0f, 60f * Time.deltaTime, 0.0f, Space.World);
+        }
+
         
     }
 }
