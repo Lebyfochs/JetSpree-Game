d99165b622bc9f48b4c1bec87880ddbcd51f52da
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2022-12-07 22:08:34 +0000

    Improved Turret Rotation
    Turret now locked to 90 degrees rotation one way or the other.

--- src/JetSpree/Assets/Scripts/TurretRotate.cs
+++ src/JetSpree/Assets/Scripts/TurretRotate.cs
@@ -10,17 +10,27 @@ public class TurretRotate : MonoBehaviour
     [SerializeField] private float sensitivity;
     [SerializeField] private Transform player;
     private float mouseX;
+    private float xRot;
     
     void Start()
     {
         sensitivity = 900.0f;
+        xRot = 0.0f;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Takes mouse X value and clamps it to 90 degrees.
+        //Turret can only move 90 degrees one way or the other.
         mouseX = Input.GetAxisRaw("Mouse X") * sensitivity * Time.deltaTime;
+        xRot += mouseX;
+        xRot = Mathf.Clamp(xRot, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(0.0f, xRot, 0.0f);
         player.Rotate(Vector3.up * mouseX);
+
         
 
     }
