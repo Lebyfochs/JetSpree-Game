b6b7f8b47bfa783f28d35ecedf886141e42bd269
Caleb Phillips <53085992+Lebyfochs@users.noreply.github.com>
2022-12-11 21:11:01 +0000

    W.I.P. projectile working
    Projectile now fires out of the turret correctly. Tweaking values later can improve the feel and experience.

--- src/JetSpree/Assets/Scripts/Projectiles/CannonProjectile.cs
+++ src/JetSpree/Assets/Scripts/Projectiles/CannonProjectile.cs
@@ -8,7 +8,7 @@ public class CannonProjectile : ProjectileBase
 
     public CannonProjectile()
     {
-        ProjectileSpeed = 25.0f;
+        ProjectileSpeed = 100.0f;
         ProjectileDamage = 15;
     }
 
@@ -23,11 +23,10 @@ public class CannonProjectile : ProjectileBase
     public override IEnumerator IMove()
     {
 
-        //Attempting to get the projectile to move based on turret's rotation. Will probably need to call an object in this script.
-        //That or find a way here to make it move forward, the cannon script instantiate it at the rotation?
+       //UPDATE: Removed Time.DeltaTime from the rb.velocity command, seems to work now.
         //transform.Translate(Vector3.up * ProjectileSpeed * Time.deltaTime);
-        rb.AddForce(transform.localPosition * ProjectileSpeed * Time.deltaTime, ForceMode.VelocityChange);
-        yield return new WaitForSeconds(2);
+        rb.velocity = transform.TransformDirection(Vector3.forward * ProjectileSpeed);
+        yield return new WaitForSeconds(1);
         Destroy(this.gameObject);
         yield return null;
     }
